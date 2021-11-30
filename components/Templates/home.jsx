98fa49fabc8803511df8c72a482c34fc38b36b50
shunki-github <shunttuan1023@gmail.com>
2021-11-30 22:10:45 +0900

    json-ld edit

--- components/Templates/home.jsx
+++ components/Templates/home.jsx
@@ -13,23 +13,19 @@ export function TempHome(asPath, locale) {
           content="MAKASERU はビジネスに寄り添った形でモバイルアプリやWebアプリ、AIなどのシステム開発やその後のサポートをします。MAKASERUの魅力は技術選定とその後のサポートです。余計な予算を削ぎ落とし、マーケティング面までサポートします。"
         />
         <link rel="icon" href="/MakaseruICO.webp" />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: `
-                        {
-                        "@context": "http://schema.org",
-                        "@type": "Product",
-                        "name": "MAKASERU",
-                        "image": "${process.env.NEXT_PUBLIC_LOGO_ICON_PATH}"
-                        "description": "MAKASERUにはシステム開発とそのマーケティングの全てを'任せる'ことができます。モバイルアプリ、業務アプリ、業務ツール、AIなど幅広い製品に対応可能です。",
-                        "brand": {
-                            "@type": "Brand",
-                            "name": "MAKASERU"
-                          }
-                        }`,
+        <script type="application/ld+json">
+          {{
+            "@context": "http://schema.org",
+            "@type": "Product",
+            "name": "MAKASERU",
+            "image": `${process.env.NEXT_PUBLIC_LOGO_ICON_PATH}`,
+            "description": "MAKASERUにはシステム開発とそのマーケティングの全てを'任せる'ことができます。モバイルアプリ、業務アプリ、業務ツール、AIなど幅広い製品に対応可能です。",
+            "brand": {
+                "@type": "Brand",
+                "name": "MAKASERU"
+            }
           }}
-        />
+        </script>
       </Head>
       <main>
         {Header(asPath, locale)}
